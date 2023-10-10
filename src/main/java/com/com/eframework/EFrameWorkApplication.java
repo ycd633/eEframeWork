8bb91b9f7eddaa255f225bc974a2845c8dbdc571
nine <ycd633@naver.com>
2023-10-10 10:06:47 +0900

    init

---
+++ src/main/java/com/com/eframework/EFrameWorkApplication.java
@@ -0,0 +1,13 @@
+package com.com.eframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFrameWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFrameWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/eframework/EFrameWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.eframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFrameWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
